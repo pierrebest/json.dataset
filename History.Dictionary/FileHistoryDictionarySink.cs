cb4ae04a3c748e9068cdc417821ab6fdb857fdd5
pierre best <pierre.best@gmail.com>
2017-04-08 18:18:07 +0100

    json file loader

---
+++ History.Dictionary/FileHistoryDictionarySink.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using History.Dictionary.Interfaces;
+
+namespace History.Dictionary
+{
+    public class FileHistoryDictionarySink : IHistoryDictionarySink
+    {
+        public bool Write(IHistoryDictionary historyDictionary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ History.Dictionary/FileHistoryDictionarySource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using History.Dictionary.Interfaces;
+
+namespace History.Dictionary
+{
+    public class FileHistoryDictionarySource : IHistoryDictionarySource
+    {
+        public bool Read(ref List<IEvent> events)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
